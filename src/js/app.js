9d528702ca7cb5039b04c496db2d55273362a514
risrobs <robertivanov22@abv.bg>
2021-12-15 02:39:28 +0200

    a

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,20 @@ window.addEventListener("DOMContentLoaded", () => {
     alert("💣");
   });
 });
+    $(function() {
+
+   // on mouse enter hide other and show current one
+   $(".coupon_hover1").mouseenter(function(event) {
+     $(".coupon_hover1").each(function() {
+       $(this).hide();
+     });
+     $(this).show();
+   });
+
+   // on mouse leave show all coupons as before
+   $(".coupon_hover1").mouseleave(function(event) {
+     $(".coupon_hover1").each(function() {
+       $(this).show();
+     });
+   });
+ })
